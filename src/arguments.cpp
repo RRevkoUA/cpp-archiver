d68414626ce503cc4a04b9a0ae65cff13f7ff601
RRevkoUA <rustamanrevko@gmail.com>
2024-11-29 19:51:00 +0200

    add compress argument

--- src/arguments.cpp
+++ src/arguments.cpp
@@ -1,8 +1,10 @@
 #include "arguments.hpp"
 #include "argparse/argparse.hpp"
 #include "main.hpp"
+#include "tar.hpp"
 
 static void show_version();
+static void compress_file(const char *const file, const char *const archive = nullptr);
 
 static argparse::ArgumentParser program(PROJECT_NAME, PROJECT_VERSION);
 
@@ -18,6 +20,12 @@ void arg_configure()
         .help("show help message")
         .default_value(false)
         .implicit_value(true);
+        
+    program.add_argument("-c", "--compress")
+        .help("compress a file or directory")
+        .default_value(false)
+        .implicit_value(true)
+        .nargs(1,2);
 }
 
 void arg_parse(int argc, const char *const argv[])
@@ -29,9 +37,25 @@ void arg_parse(int argc, const char *const argv[])
         show_version();
         std::exit(0);
     }
+    else if (program.is_used("--compress"))
+    {
+        auto files = program.get<std::vector<std::string>>("--compress");
+        const char *dir = files[0].c_str();
+        const char *archive = files.size() == 2 ? files[1].c_str() : nullptr;
+        compress_file(dir, archive);
+    }
+    else
+    {
+        std::cout << program;
+    }
 }
 
 static void show_version()
 {
     std::cout << "Version: " << PROJECT_VERSION << std::endl;
+}
+
+static void compress_file(const char *const file, const char *const archive)
+{
+    tar_compress(file, archive);
 }
