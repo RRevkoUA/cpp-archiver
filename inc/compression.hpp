426b5b3b3bdc2b39f5ffa88412ac8c0d26d7d794
RRevkoUA <rustamanrevko@gmail.com>
2024-11-29 23:08:43 +0200

    fix: move enum to the top of the file

--- inc/compression.hpp
+++ inc/compression.hpp
@@ -1,18 +1,6 @@
 #ifndef COMPRESSION_HPP
 #define COMPRESSION_HPP
 
-// Compress a tar file.
-// src: The file or dir to be compress.
-// dist: The archive file to be created (optional).
-// type: The type of compression (GZ by default).
-void compression_compress(const char *const src, const char *const tar = nullptr, const compression_type_t type = TAR_GZ);
-
-// Decompress a tar file.
-// src: The tar archive to be extracted.
-// dir: The directory to be created (optional).
-// type: The type of compression (autosearch, if empty).
-void compression_extract(const char *const tar, const char *const dir = nullptr, const compression_type_t type = UNKNOWN);
-
 typedef enum {
     TAR_GZ = 0,
     TAR_BZ2,
@@ -25,5 +13,17 @@ typedef enum {
     TAR_7Z,
     UNKNOWN = -1
 } compression_type_t; 
+
+// Compress a tar file.
+// src: The file or dir to be compress.
+// dist: The archive file to be created (optional).
+// type: The type of compression (GZ by default).
+void compression_compress(const char *const src, const char *const tar = nullptr, const compression_type_t type = TAR_GZ);
+
+// Decompress a tar file.
+// src: The tar archive to be extracted.
+// dir: The directory to be created (optional).
+// type: The type of compression (autosearch, if empty).
+void compression_extract(const char *const tar, const char *const dir = nullptr, const compression_type_t type = UNKNOWN);
 
 #endif // COMPRESSION_HPP
